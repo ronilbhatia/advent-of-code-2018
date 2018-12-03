eed8a12616ff94c7ee1c8e9ec656cf7f4f3acfb5
Ronil Bhatia <ronilbhatia@gmail.com>
2018-12-02 21:26:09 -0800

    Initial commit for first two days of coding challenges

---
+++ 12-1-18/frequency_calculator.rb
@@ -0,0 +1,43 @@
+require 'byebug'
+#
+# def frequency_calculator(file)
+#   frequencies = File.readlines(file).map(&:chomp)
+#   frequencies.reduce(0) do |sum, delta|
+#     direction = delta[0]
+#     magnitude = delta[1..-1].to_i
+#     if direction == "+"
+#       sum + magnitude
+#     else
+#       sum - magnitude
+#     end
+#   end
+# end
+
+def first_frequency_reached_twice(file)
+  frequency_deltas = File.readlines(file).map(&:chomp)
+  frequencies = Hash.new(0)
+  current_frequency = 0
+
+  while true
+    frequency_deltas.each do |delta|
+      direction = delta[0]
+      magnitude = delta[1..-1].to_i
+      frequencies[current_frequency] += 1
+
+      if frequencies[current_frequency] == 2
+        return current_frequency
+      end
+
+      if direction == "+"
+        current_frequency += magnitude
+      else
+        current_frequency -= magnitude
+      end
+    end
+  end
+
+  return current_frequency
+end
+if __FILE__ == $PROGRAM_NAME
+  puts first_frequency_reached_twice("input.txt")
+end
---
+++ 12-2-18/checksum.rb
@@ -0,0 +1,76 @@
+require 'byebug'
+
+def checksum(file)
+  words = File.readlines(file).map(&:chomp)
+  twos = 0
+  threes = 0
+
+  words.each do |word|
+    debugger
+    res = twos_and_threes(word)
+
+    twos += 1  if res['two']
+    threes += 1 if res['three']
+  end
+
+  twos * threes
+end
+
+def twos_and_threes(str)
+  letters = Hash.new(0)
+
+  str.each_char { |char| letters[char] += 1 }
+
+  res = Hash.new(false)
+
+  res['two'] = true if letters.any? { |_, count| count == 2 }
+  res['three'] = true if letters.any? { |_, count| count == 3 }
+
+  res
+end
+
+def find_correct_boxes(file)
+  words = File.readlines(file).map(&:chomp)
+
+  i = 0
+
+  while i < words.length
+    j = i + 1
+
+    while j < words.length
+      res = compare_strings(words[i], words[j])
+
+      if res
+        word = words[i]
+        debugger
+        str = ''
+        str += word[0...res]
+        str += word[i+1..-1] unless word[i+1..-1].nil?
+        return str
+      end
+
+      j += 1
+    end
+
+    i +=1
+  end
+end
+
+def compare_strings(str1, str2)
+  diffs = 0
+  split_idx = 0
+
+  str1.chars.each_index do |i|
+    if str1[i] != str2[i]
+      diffs += 1
+      split_idx = i
+    end
+  end
+
+  return split_idx if diffs == 1
+  false
+end
+
+if __FILE__ == $PROGRAM_NAME
+  puts find_correct_boxes("input.txt")
+end
